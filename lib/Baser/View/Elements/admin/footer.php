0ba82f0b8e111d4a8862825179031b3c56aa36c8
gondoh <gondoh@gufii.net>
2014-11-01 17:06:59 +0900

    管理画面フッタのID,class名プリフィックス対応

--- lib/Baser/View/Elements/admin/footer.php
+++ lib/Baser/View/Elements/admin/footer.php
@@ -12,16 +12,16 @@
  * @license			http://basercms.net/license/index.html
  */
 ?>
-<div id="Footer">
+<div class="bc-footer">
 
-	<div id="FooterInner" class="clearfix">
+	<div class="clearfix bc-footer-inner">
 
-		<div id="FooterLogo">
+		<div class="bc-footer-logo">
 			<?php $this->BcBaser->link($this->BcBaser->getImg('admin/logo_footer.png', array('width' => 155, 'height' => 30, 'alt' => 'baserCMSロゴ')), array('controller' => 'dashboard', 'action' => 'index')) ?>
 		</div>
 
 		<?php if (!empty($user)): ?>
-			<div id="FooterMenu">
+			<div class="bc-footer-menu">
 				<h2><?php $this->BcBaser->img('admin/head_menu.png', array('width' => 44, 'height' => 16, 'alt' => 'MENU')) ?></h2>
 				<ul>
 					<li><?php $this->BcBaser->link('固定ページ管理', array('plugin' => '', 'controller' => 'pages', 'action' => 'index')) ?></li>
@@ -33,7 +33,7 @@
 			</div>
 		<?php endif ?>
 
-		<div id="FooterLink">
+		<div class="bc-footer-link">
 			<h2><?php $this->BcBaser->img('admin/head_link.png', array('width' => 36, 'height' => 16, 'alt' => 'LINK')) ?></h2>
 			<ul>
 				<li><a href="http://basercms.net/" target="_blank">baserCMS 公式サイト</a></li>
@@ -46,12 +46,12 @@
 		</div>
 
 		<div class="float-right">
-			<ul id="FooterBanner">
+			<ul class="bc-footer-banner">
 				<li><?php $this->BcBaser->link($this->BcBaser->getImg('baser.power.gif', array('alt' => 'baserCMS Power')), 'http://basercms.net/', array('target' => '_blank', 'title' => 'baserCMS Power')) ?></li>
 				<li><?php $this->BcBaser->link($this->BcBaser->getImg('cake.power.gif', array('alt' => 'CakePHP Power')), 'http://cakephp.jp/', array('target' => '_blank', 'title' => 'CakePHP Power')) ?></li>
 			</ul>
-			<p id="BaserVersion">baserCMS <?php echo $baserVersion ?></p>
-			<div id="Copyright">Copyright (C) baserCMS Users Community <?php $this->BcBaser->copyYear(2009) ?> All rights reserved.</div>
+			<p class="bc-version">baserCMS <?php echo $baserVersion ?></p>
+			<div class="bc-copyright">Copyright (C) baserCMS Users Community <?php $this->BcBaser->copyYear(2009) ?> All rights reserved.</div>
 		</div>
 
 		<!-- / #FooterInner --></div>
