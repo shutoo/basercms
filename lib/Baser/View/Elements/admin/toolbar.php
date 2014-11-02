6299adf42ec3a8bf22027e4d9edb1c4e86fa4423
dzero-staff <hirao+git-staff@d-zero.co.jp>
2014-11-02 09:15:50 +0900

    jquery.fixedMenu.jsを使用しないので削除

--- lib/Baser/View/Elements/admin/toolbar.php
+++ lib/Baser/View/Elements/admin/toolbar.php
@@ -12,7 +12,7 @@
  * @license			http://basercms.net/license/index.html
  */
 App::uses('AuthComponent', 'Controller/Component');
-$this->BcBaser->js(array('admin/outerClick', 'admin/jquery.fixedMenu'));
+$this->BcBaser->js(array('admin/outerClick'));
 $loginUrl = '';
 $currentAuthPrefix = Configure::read('BcAuthPrefix.' . $currentPrefix);
 if (!empty($currentAuthPrefix['loginAction'])) {
